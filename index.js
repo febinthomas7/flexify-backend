13e5493cbeb89b50ebd290ec4268c5c5bbb4adc4
febin thomas <tfebin39@gmail.com>
2024-07-21 23:14:55 +0530

    port fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const axios = require("axios");
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 const api_key = process.env.API_KEY;
 
 app.get("/api/movies", (req, res) => {
